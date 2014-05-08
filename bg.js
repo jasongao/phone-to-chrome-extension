6a6696e3d2b0536f73669fdc0a8d8ae13d630134
Amaan Cheval <amaan.1996@gmail.com>
2014-05-08 22:35:27 +0530

    Updated to store passphrase on server

--- bg.js
+++ bg.js
@@ -4,13 +4,17 @@ var ws;
 var host = 'ws://mobile-to-chrome.herokuapp.com/';
 // var host = 'ws://localhost/';
 
-var registerID = function (ws) {
+var registerID = function () {
     ws.send(JSON.stringify({
         command: 'setID',
-        id: localStorage.id
+        id: localStorage.id,
+        pass: localStorage.pass
     }));
+    console.log('Registered ID');
 };
-var tabHistory = [];
+var tabHistory = []; // Holds the actual tabs after they're loaded
+var tabIDs = []; // Holds IDs of tabs that are created. Checked against when they load
+
 try {
     tabHistory = JSON.parse(localStorage.tabHistory);
 }
@@ -19,23 +23,34 @@ catch (e) {
 }
 
 var addTab = function (tab) {
+    tab.timeString = moment().format('DD/MM/YYYY');
     tabHistory.push(tab);
+    console.log(tab.status, tab.favIconUrl);
     if (tabHistory.length > 10) {
         tabHistory = tabHistory.slice(tabHistory.length - 10, tabHistory.length);
     }
     localStorage.tabHistory = JSON.stringify(tabHistory);
 };
 
+chrome.tabs.onUpdated.addListener(function (id, changeInfo, tab) {
+    var index = tabIDs.indexOf(id);
+    if (changeInfo.status === 'complete' && index !== -1) {
+        addTab(tab);
+        tabIDs.splice(index, 1);
+    }
+});
+
 var createWS = function (){
 
     ws = new WebSocket(host);
     ws.onopen = function () {
         console.log('Opened');
-        registerID(ws);
+        registerID();
     };
     ws.onmessage = function (message) {
         console.log('Received', message);
         var obj = {};
+        var url;
         try {
             obj = JSON.parse(message.data);
         }
@@ -50,11 +65,26 @@ var createWS = function (){
         }
         if (obj.url){
             if (localStorage.pass === obj.pass) {
+                url = obj.url;
+                var protocol = url.match(/\w+\:\/*.*/);
+                var textLink = url.match(/[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,3}/);
+                // If the protocol wasn't found, or if it was found as part of the text, instead of being the entire text
+                if (!protocol || protocol[0] !== url) {
+                    if (textLink && textLink[0] === url) {
+                        url = 'http://' + url;
+                    }
+                    else {
+                        url = 'http://google.com/#q=' + url;
+                    }
+                }
                 chrome.tabs.create({
-                    url: obj.url
+                    url: url
                 },
                 function (tab) {
-                    addTab(tab);
+                    tabIDs.push(tab.id);
+                    if (tabIDs.length > 10) {
+                        tabIDs = tabIDs.slice(tabIDs.length - 10, tabIDs.length);
+                    }
                 });
                 // Respond and let the server know that everything worked
                 ws.send(JSON.stringify({
--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 (function () {
+    var background = chrome.extension.getBackgroundPage();
+
     var uid = document.getElementById('uid');
     uid.value = localStorage.id;
 
@@ -24,5 +26,52 @@
         e.preventDefault();
         localStorage.pass = pass.value;
         save.disabled = true;
+        background.registerID();
     }, false);
+
+    var history = document.getElementById('history');
+    window.addEntry = function (favico, title, url, timeString) {
+        var entry = document.createElement('div');
+        entry.className = 'entry';
+
+        var img = document.createElement('img');
+        img.src = favico ? favico : 'favicon.png';
+        img.width = '16';
+        img.className = 'favico';
+
+        var titleElem = document.createElement('span');
+        titleElem.className = 'urlTitle';
+        titleElem.textContent = title || 'Untitled';
+
+        var urlElem = document.createElement('span');
+        urlElem.className = 'url';
+        urlElem.textContent = url;
+
+        var timeElem = document.createElement('span');
+        timeElem.className = 'time';
+        timeElem.textContent = timeString;
+
+        entry.addEventListener('click', function (e) {
+            chrome.tabs.create({
+                url: url
+            });
+        }, false);
+
+        entry.appendChild(img);
+        entry.appendChild(titleElem);
+        entry.appendChild(urlElem);
+        entry.appendChild(timeElem);
+
+        history.appendChild(entry);
+    };
+
+    if (localStorage.tabHistory) {
+        var tabHistory = JSON.parse(localStorage.tabHistory);
+        tabHistory.reverse().forEach(function (tab) {
+            addEntry(tab.favIconUrl, tab.title, tab.url, tab.timeString);
+        });
+    }
+    else {
+        document.getElementById('nohistory').style.display = 'block';
+    }
 })();
